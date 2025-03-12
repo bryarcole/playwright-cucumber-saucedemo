73cbc5f084f5a188dabba9b9ecb2a9059abeeb2a
Bryar Cole <bryarcole@gmail.com>
2025-03-12 17:30:33 -0500

    updateing

--- steps/login.steps.ts
+++ steps/login.steps.ts
@@ -11,20 +11,20 @@ Given('I open the Sauce Demo login page', async function () {
     await page.goto('https://www.saucedemo.com/');
 });
 
-When('I enter valid credentials', async function () {
+When('Successful login with valid credentials', async function () {
     await page.fill('#user-name', 'standard_user');
     await page.fill('#password', 'secret_sauce');
     await page.click('#login-button');
+});
+
+Then('I should be redirected to the products page', async function () {
+  await expect(page).toHaveURL(/inventory.html/);
 });
 
 When('I enter invalid credentials', async function () {
     await page.fill('#user-name', 'invalid_user');
     await page.fill('#password', 'wrong_password');
     await page.click('#login-button');
-});
-
-Then('I should be redirected to the products page', async function () {
-    await expect(page).toHaveURL(/inventory.html/);
 });
 
 Then('I should see an error message', async function () {
